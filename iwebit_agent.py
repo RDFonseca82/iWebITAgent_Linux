43cdc9988f5d05a1014e43f30102ce40708c8fd5
RDFonseca.com <38105077+RDFonseca82@users.noreply.github.com>
2025-07-30 08:22:56 +0100

    Update iwebit_agent.py

--- iwebit_agent.py
+++ iwebit_agent.py
@@ -16,7 +16,7 @@ from datetime import datetime
 # =================== CONFIG ===================
 CONFIG_FILE = '/opt/iwebit_agent/iwebit_agent.conf'
 # UNIQUEID_FILE = '/opt/iwebit_agent/uniqueid.conf'
-VERSION = '1.0.23.1'
+VERSION = '1.0.24.1'
 LOG_ENABLED = True
 LOG_FILE = '/var/log/iwebit_agent/iwebit_agent.log'
 UPDATE_URL = 'https://raw.githubusercontent.com/RDFonseca82/iWebITAgent_Linux/main/iwebit_agent.py'
