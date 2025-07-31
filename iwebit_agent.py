3700586ddcf538d794caa10148898f341b8e5fcd
RDFonseca.com <38105077+RDFonseca82@users.noreply.github.com>
2025-07-31 10:25:15 +0100

    Update iwebit_agent.py

--- iwebit_agent.py
+++ iwebit_agent.py
@@ -16,7 +16,7 @@ from datetime import datetime
 # =================== CONFIG ===================
 CONFIG_FILE = '/opt/iwebit_agent/iwebit_agent.conf'
 # UNIQUEID_FILE = '/opt/iwebit_agent/uniqueid.conf'
-VERSION = '1.0.30.1'
+VERSION = '1.0.31.1'
 LOG_ENABLED = True
 LOG_FILE = '/var/log/iwebit_agent/iwebit_agent.log'
 UPDATE_URL = 'https://raw.githubusercontent.com/RDFonseca82/iWebITAgent_Linux/main/iwebit_agent.py'
@@ -170,10 +170,8 @@ def get_device_type():
 import os
 
 def is_reboot_pending():
-    """
-    Verifica se o sistema Linux tem um reboot pendente.
-    Retorna True se sim, False se não.
-    """
+    # Verifica se o sistema Linux tem um reboot pendente. Retorna True se sim, False se não.
+
     reboot_files = [
         "/var/run/reboot-required",                  # Debian/Ubuntu
         "/run/reboot-required",                      # Alguns derivados
